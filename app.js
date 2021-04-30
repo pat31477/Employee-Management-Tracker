320023ae09211de7fcbaaf278a1935dc0e75f5d0
Pat31477 <pat31477@yahoo.com>
2021-04-30 19:07:13 -0400

    updating app.js with core application loop logic

--- app.js
+++ app.js
@@ -29,3 +29,40 @@ function welcome(){
         loadMainMenu();       
     }, endTime); 
 }
+//core application loop:
+async function loadMainMenu(){
+
+    await inquirer.prompt(questions.mainMenu)
+    .then(async function(answers){
+        switch(answers.choice){
+            case "View All Employees":
+                await getAllEmployeesFullData();                
+                break;
+            case "View Departments":
+                await viewAllDepartments();                
+                break;
+            case "View Roles":
+                await viewAllRoles();                
+                break;
+            case "View employees by Manager":
+                await viewEmployeesByManager();
+                break;
+            case "Add a Department":
+                await addDepartment();
+                break;
+            case "Add a Role":
+                await addRole();
+                break;
+            case "Update Employee Role":
+                await updateEmployeeRole();
+                break;
+            case "Add an Employee":
+                await addEmployee();
+                break;
+            case "Quit":     
+                break;
+            default:
+                break;
+        }            
+    });    
+}
